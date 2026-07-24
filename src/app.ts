ad763d316a12b565b5d908d01a7f0b78ec83c915
Saddam <eyarinsultana@gmail.com>
2026-07-24 08:33:36 +0600

    app and server route create

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import config from "./config";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { notFound } from "./middlewares/notFound";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+const endpointSecret = config.stripe_webhook_secret;
+
+app.use("/api/subscription/webhook", express.raw({ type: "application/json" }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+// app.post()
+
+app.use(notFound);
+
+app.use(globalErrorHandler);
+
+export default app;
